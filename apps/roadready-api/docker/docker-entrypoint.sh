703f10f1c049734fd40145d1b9b29dfe04703998
Ravi Shankar <ravioshankar@outlook.com>
2026-08-06 22:31:11 -0500

    remove apps folder

--- apps/roadready-api/docker/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -e
-cd /app
-
-echo "Running database migrations (alembic upgrade head)..."
-alembic upgrade head
-
-echo "Starting uvicorn on 0.0.0.0:8888..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8888
